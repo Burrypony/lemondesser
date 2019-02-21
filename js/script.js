c6ad3091aaea7054cdd9c8995c6c0cad7e9e8d17
Burrypony <kiril.chernysh@gmail.com>
2019-02-21 23:54:27 +0200

    wokring

--- js/script.js
+++ js/script.js
@@ -4,4 +4,29 @@ $(document).ready(function () {
 
     $('.animated-icon1').toggleClass('open');
   });
+
+  $('#employer').on('click', function () {
+    $('#employer').addClass("section7BlocksHover"),
+    $('#investor').removeClass("section7BlocksHover"),
+    $('#startaper').removeClass("section7BlocksHover"),
+    $('#expert').removeClass("section7BlocksHover");
+  });
+  $('#investor').on('click', function () {
+    $('#employer').removeClass("section7BlocksHover"),
+    $('#investor').addClass("section7BlocksHover"),
+    $('#startaper').removeClass("section7BlocksHover"),
+    $('#expert').removeClass("section7BlocksHover");
+  });
+  $('#startaper').on('click', function () {
+    $('#employer').removeClass("section7BlocksHover"),
+    $('#investor').removeClass("section7BlocksHover"),
+    $('#startaper').addClass("section7BlocksHover"),
+    $('#expert').removeClass("section7BlocksHover");
+  });
+  $('#expert').on('click', function () {
+    $('#employer').removeClass("section7BlocksHover"),
+    $('#investor').removeClass("section7BlocksHover"),
+    $('#startaper').removeClass("section7BlocksHover"),
+    $('#expert').addClass("section7BlocksHover");
+  });
 });
