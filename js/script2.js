9cfa28536db891429f7209c2497cf91b79a2fc28
Burrypony <kiril.chernysh@gmail.com>
2019-03-28 20:34:30 +0200

    biznes,investor, startap mobile version

--- js/script2.js
+++ js/script2.js
@@ -117,13 +117,54 @@ $(document).ready(function() {
 
   $('#firstInvTable').on('click', function(){
     $("#invTable2").removeClass('dislayNone');
-    $(".invTable2").css("margin-top", "226px");
+    if($(window).width() > 1024)
+    {
+      $(".invTable2").css("margin-top", "220px");
+    }
+    if(($(window).width() > 860)&&($(window).width() < 1024))
+    {
+      $(".invTable2").css("margin-top", "690px");
+    }
+    if(($(window).width() > 810)&&($(window).width() < 860))
+    {
+      $(".invTable2").css("margin-top", "570px");
+    }
+    if(($(window).width() > 480)&&($(window).width() < 810))
+    {
+      $(".invTable2").css("margin-top", "530px");
+    }
+    if(($(window).width() > 390)&&($(window).width() < 480))
+    {
+      $(".invTable2").css("margin-top", "480px");
+    }
+    if($(window).width() < 390){
+      $(".invTable2").css("margin-top", "500px");
+    }
     $("#section7InvTables1").addClass("section7Inv1000px");
   })
 
   $('#secondInvTable').on('click', function(){
     $("#invTable3").removeClass('dislayNone');
-    $(".invTable3").css("margin-top", "220px");
+    if($(window).width() >= 1024)
+    {
+      $(".invTable3").css("margin-top", "220px");
+    }
+    if(($(window).width() > 860)&&($(window).width() < 1024))
+    {
+      $(".invTable3").css("margin-top", "700px");
+    }
+    if(($(window).width() > 480)&&($(window).width() < 860))
+    {
+      $(".invTable3").css("margin-top", "575px");
+    }
+    if(($(window).width() > 390)&&($(window).width() < 480))
+    {
+      $(".invTable3").css("margin-top", "485px");
+    }
+    if($(window).width() < 390){
+      $(".invTable3").css("margin-top", "560px");
+    }
+
     $("#section7InvTables2").addClass("section7Inv1000px");
   })
 
